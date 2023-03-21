89623191ae14864fba6840f55a78da6c770f817c
Santosh Kumar <santosh.283143@gmail.com>
2023-03-21 16:45:26 +0530

    added material ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Button } from "@mui/material";
 import React from "react";
 
 const App = () => {
     return (
         <div>
             <h1 className="text-info text-center mt-5 display-2 fw-bold">Start With Your React App</h1>
+            <Button variant="contained">Click here</Button>
         </div>
     )
 }
